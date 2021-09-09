9c6353ae73e979ee79e19c20e89efeeb0d00c219
Odysseas Lamtzidis <odyslam@gmail.com>
2021-09-09 18:14:45 +0300

    feat: gas estimate, contract_size

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,54 +1,150 @@
+#!/usr/bin/env bash
+
+# All contracts are output to `out/addresses.json` by default
+OUT_DIR=${OUT_DIR:-$PWD/out}
+ADDRESSES_FILE=${ADDRESSES_FILE:-$OUT_DIR/"addresses.json"}
+# default to localhost rpc
+ETH_RPC_URL=${ETH_RPC_URL:-http://localhost:8545}
+
+# green log helper
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-export OUT_DIR=${OUT_DIR:-$PWD/out}
-ADDRESSES_FILE="$OUT_DIR/addresses.json"
-export CONFIG_FILE="${OUT_DIR}/config.json"
-
-deploy() {
-    NAME=$1
-    SIG=$2
-    ARGS=${@:3}
-
-    # get the bytecode from the compiled file
-    PATTERN=".contracts[\"src/$NAME.sol\"].$NAME.evm.bytecode.object"
-    BYTECODE=0x$(cat out/dapp.sol.json | jq -r "$PATTERN")
-
-    # estimate gas
-    GAS=$(seth estimate --create $BYTECODE $SIG $ARGS --rpc-url $RPC_URL --from $FROM)
-
-    # deploy
-    ADDRESS=$(dapp create $NAME $ARGS -- --rpc-url $RPC_URL --from $FROM --gas $GAS)
-    echo $ADDRESS
-}
-
-# loads addresses as key-value pairs from $ADDRESSES_FILE and exports them as
-# environment variables.
-loadAddresses() {
-    local keys
-
-    kys=$(jq -r "keys_unsorted[]" "$ADDRESSES_FILE")
-    for KEY in $keys; do
-        VALUE=$(jq -r ".$KEY" "$ADDRESSES_FILE")
-        export "$KEY"="$VALUE"
-    done
-}
-
-# concatenates the args with a comma
-join() {
-    local IFS=","
-    echo "$*"
-}
-
+if command -v tput > /dev/null 2>&1; then
+  if [ $(($(tput colors 2> /dev/null))) -ge 8 ]; then
+    # Enable colors
+    TPUT_RESET="$(tput sgr 0)"
+    TPUT_YELLOW="$(tput setaf 3)"
+    TPUT_RED="$(tput setaf 1)"
+    TPUT_BLUE="$(tput setaf 4)"
+    TPUT_GREEN="$(tput setaf 2)"
+    TPUT_WHITE="$(tput setaf 7)"
+    TPUT_BOLD="$(tput bold)"
+  fi
+fi
 log() {
     printf '%b\n' "${GREEN}${*}${NC}"
     echo ""
 }
 
-toUpper() {
-    echo "$1" | tr '[:lower:]' '[:upper:]'
+# ensure ETH_FROM is set and give a meaningful error message
+if [[ -z ${ETH_FROM} ]]; then
+    echo "ETH_FROM not found, please set it and re-run the last command."
+    exit 1
+fi
+
+# Setup addresses file
+cat > "$ADDRESSES_FILE" <<EOF
+{
+    "DEPLOYER": "$(seth --to-checksum-address "$ETH_FROM")"
+}
+EOF
+
+# Call as `ETH_FROM=0x... ETH_RPC_URL=<url> deploy ContractName arg1 arg2 arg3`
+# (or omit the env vars if you have already set them)
+
+estimate_gas() {
+    NAME=$1
+    ARGS=${@:2}
+    # select the filename and the contract in it
+    PATTERN=".contracts[\"src/$NAME.sol\"].$NAME"
+
+    # get the constructor's signature
+    ABI=$(jq -r "$PATTERN.abi" out/dapp.sol.json)
+    SIG=$(echo $ABI | seth --abi-constructor)
+
+    # get the bytecode from the compiled file
+    BYTECODE=0x$(jq -r "$PATTERN.evm.bytecode.object" out/dapp.sol.json)
+    # estimate gas
+    GAS=$(seth estimate --create $BYTECODE $SIG $ARGS --rpc-url $ETH_RPC_URL)
+
+    GASNOW_RESPONSE=$(curl -s https://www.gasnow.org/api/v3/gas/price)
+    response=$(jq '.code' <<< $GASNOW_RESPONSE)
+    if [[ $response != "200" ]]; then
+      echo "Could not get gas information from ${TPUT_BOLD}gasnow.org${TPUT_RESET}: https://www.gasnow.org"
+      echo "response code: $response"
+   else
+     rapid=$(( $(jq '.data.rapid' <<< $GASNOW_RESPONSE) / 1000000000 ))
+     fast=$(( $(jq '.data.fast' <<< $GASNOW_RESPONSE) / 1000000000 ))
+     standard=$(( $(jq '.data.standard' <<< $GASNOW_RESPONSE) / 1000000000 ))
+     slow=$(( $(jq '.data.slow' <<< $GASNOW_RESPONSE) / 1000000000 ))
+     echo "Gas prices from ${TPUT_BOLD}gasnow.org${TPUT_RESET}: https://www.gasnow.org"
+     echo " \
+     ${TPUT_RED}Rapid: $rapid gwei ${TPUT_RESET} \n
+     ${TPUT_YELLOW}Fast: $fast gwei \n
+     ${TPUT_BLUE}Standard: $standard gwei \n
+     ${TPUT_GREEN}Slow: $slow gwei${TPUT_RESET}" | column -t
+     echo "Estimated Gas cost for deployment of $NAME: ${TPUT_BOLD}$GAS${TPUT_RESET} units of gas"
+     echo "Total cost for deployment:"
+     rapid_cost=$(echo "scale=5; $GAS*$rapid/1000000000" | bc)
+     fast_cost=$(echo "scale=5; $GAS*$fast/1000000000" | bc)
+     standard_cost=$(echo "scale=5; $GAS*$standard/1000000000" | bc)
+     slow_cost=$(echo "scale=5; $GAS*$slow/1000000000" | bc)
+     echo " \
+     ${TPUT_RED}Rapid: $rapid_cost ETH ${TPUT_RESET} \n
+     ${TPUT_YELLOW}Fast: $fast_cost ETH \n
+     ${TPUT_BLUE}Standard: $standard_cost ETH \n
+     ${TPUT_GREEN}Slow: $slow_cost ETH ${TPUT_RESET}" | column -t
+  fi
 }
 
-toLower() {
-    echo "$1" | tr '[:upper:]' '[:lower:]'
-}e
+contract_size(){
+    NAME=$1
+    ARGS=${@:2}
+    # select the filename and the contract in it
+    PATTERN=".contracts[\"src/$NAME.sol\"].$NAME"
+
+    # get the constructor's signature
+    ABI=$(jq -r "$PATTERN.abi" out/dapp.sol.json)
+    SIG=$(echo $ABI | seth --abi-constructor)
+
+    # get the bytecode from the compiled file
+    BYTECODE=0x$(jq -r "$PATTERN.evm.bytecode.object" out/dapp.sol.json)
+    length=$(echo $BYTECODE | wc -m )
+    echo "${NAME} contract size: $(( $length / 2 )) bytes"
+    echo "$(( 24576 - $length / 2 )) bytes to reach the smart contract size limit of 24576 bytes."
+}
+
+
+
+deploy() {
+    NAME=$1
+    ARGS=${@:2}
+    # select the filename and the contract in it
+    PATTERN=".contracts[\"src/$NAME.sol\"].$NAME"
+
+    # get the constructor's signature
+    ABI=$(jq -r "$PATTERN.abi" out/dapp.sol.json)
+    SIG=$(echo $ABI | seth --abi-constructor)
+
+    # get the bytecode from the compiled file
+    BYTECODE=0x$(jq -r "$PATTERN.evm.bytecode.object" out/dapp.sol.json)
+#
+#
+    # estimate gas
+    GAS=$(seth estimate --create $BYTECODE $SIG $ARGS --rpc-url $ETH_RPC_URL)
+
+    # deploy
+    ADDRESS=$(dapp create $NAME $ARGS -- --gas $GAS --rpc-url $ETH_RPC_URL)
+
+    # save the addrs to the json
+    # TODO: It'd be nice if we could evolve this into a minimal versioning system
+    # e.g. via commit / chainid etc.
+    saveContract $NAME $ADDRESS
+
+    echo $ADDRESS
+}
+
+# Call as `saveContract ContractName 0xYourAddress` to store the contract name
+# & address to the addresses json file
+
+
+
+saveContract() {
+    # create an empty json if it does not exist
+    if [[ ! -e $ADDRESSES_FILE ]]; then
+        echo "{}" > $ADDRESSES_FILE
+    fi
+    result=$(cat $ADDRESSES_FILE | jq -r ". + {\"$1\": \"$2\"}")
+    printf %s "$result" > "$ADDRESSES_FILE"
+}
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,18 @@
-. ./scripts/common.sh
+. $(dirname $0)/common.sh
 
 # default to localhost rpc
 RPC_URL=${ETH_RPC_URL:-http://localhost:8545}
 
 # Mainnet loot address
 # Default to it if nothing is provided
-LOOT="${LOOT:-$MAINNET_LOOT}"
-
 # Deploy.
-LootLooseAddr=$(deploy LootLoose 'LootLoose(address)' $LOOT)
-log "LootLoose deployed at:" $LootLooseAddr
-
+LoadedAddress=$(deploy ${contract});
+log "Loaded deployed at:" $LoadedAddress
+export LOADED=$LoadedADdress
 # Log addresses to file
 cat > "$OUT_DIR"/addresses.json <<EOF
 {
     "DEPLOYER": "$(seth --to-checksum-address "$FROM")",
-    "LOOTLOOSE": "$LootLooseAddr",
+    "LOADED": "$LoadedAddress",
 }
 EOF
